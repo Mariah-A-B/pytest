5f8fd1adf4025f3d1c76e64648eff1bb1ec7fcba
Mariah Beaudoin <mab2nbn@gmail.com>
2022-11-09 14:52:09 -0500

    changed test

--- test_1.py
+++ test_1.py
@@ -1,16 +1,3 @@
-import unittest
-from fractions import Fraction
-from my_sum import sum
-
-
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
-        """
-        Test that it can sum a list of integers
-        """
-        data = [1, 2, 3]
-        result = sum(data)
-        self.assertEqual(result, 6)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_easy():
+    print("hi")
+    assert 1 == 1
